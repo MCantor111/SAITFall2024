29eb792ed4eae01e1a06d66afc3d727d7f7cd3e3
MCantor111 <matthew.zapalski@edu.sait.ca>
2024-10-30 20:50:39 -0600

    RAD1: Added Some Elements

--- OOSD/Assignments/CPRG200_RAD/CPRG200Assignment1_Cantor/CPRG200Assignment1_Cantor/obj/Debug/net8.0-windows/CPRG200Assignment1_Cantor.AssemblyInfo.cs
+++ OOSD/Assignments/CPRG200_RAD/CPRG200Assignment1_Cantor/CPRG200Assignment1_Cantor/obj/Debug/net8.0-windows/CPRG200Assignment1_Cantor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPRG200Assignment1_Cantor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab73824300b989a84c2e3445fa96e985e1228c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+891d433250b17f8767182b1427fc8df84db06056")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPRG200Assignment1_Cantor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CPRG200Assignment1_Cantor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
