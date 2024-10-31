d4ce2e41030e6685d68dd5b8719652e158d79c76
MCantor111 <matthew.zapalski@edu.sait.ca>
2024-10-31 10:32:07 -0600

    RAD1: RESTARTED PROJECT
    kurwa, kurwa, kurwa. cholera, dlaczego ja?

---
+++ OOSD/Assignments/CPRG200_RAD/CPRG200Assignment1_Cantor_try2/Lunch Order.cs
@@ -0,0 +1,10 @@
+namespace RAD2024_Cantor_Assignment1_try2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
